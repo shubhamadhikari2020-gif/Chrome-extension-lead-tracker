3f156b0c24b2817230595ac33dbeb068f51dca0d
Shubham <shubhamadhikari2020@gmail.com>
2026-05-17 20:28:03 +0530

    show's history everytime the button is clicked

--- extension.js
+++ extension.js
@@ -1,5 +1,16 @@
-let inputBtn = document.getElementById("input-btn");
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
-  console.log("Button clicked from addEventListener");
-});
+  myLeads.push(inputEl.value); // Pushes input values into myLeads array
+  console.log(myLeads);
+  inputEl.value = "";
+  ulEl.innerHTML = ""; // Clears the previous list items from the screen
+
+  // Moving the loop INSIDE so it triggers every time the button is clicked!
+  for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+  }
+}); // <--- The event listener now ends here, safely protecting the loop!
