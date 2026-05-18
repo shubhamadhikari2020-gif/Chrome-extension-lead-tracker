cad8c70985fd3c5d596637262d12dd5ad65797f5
Shubham <shubhamadhikari2020@gmail.com>
2026-05-18 13:59:16 +0530

    Build the list as a text string first to update the webpage only once instead of on every loop.

--- extension.js
+++ extension.js
@@ -10,12 +10,15 @@ inputBtn.addEventListener("click", function() {
   ulEl.innerHTML = ""; // Clears the previous list items from the screen
 
   // Moving the loop INSIDE so it triggers every time the button is clicked!
-  
+  let listItems= " "
   for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+
     //OTHER METHOD FOR THE ABOVE LINE:
     //const li= document.createElement("li")
     //li.textContent=myLeads[i]
     //ulEl.append(li)
+    listItems += "<li>"+ myLeads[i] +"</li>"
   }
+  ulEl.innerHTML= listItems;
 });   
