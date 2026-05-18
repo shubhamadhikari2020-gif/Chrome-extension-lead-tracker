3f2771a9dbcd5a05eda14989276fcce057139b88
Shubham <shubhamadhikari2020@gmail.com>
2026-05-18 14:27:20 +0530

    Leads inputs can now  handle heavy traffic, increasing performance

--- extension.js
+++ extension.js
@@ -5,20 +5,11 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value); // Pushes input values into myLeads array
-  console.log(myLeads);
-  inputEl.value = "";
-  ulEl.innerHTML = ""; // Clears the previous list items from the screen
+  renderLead()
+  inputEl.value=""
+})
 
-  // Moving the loop INSIDE so it triggers every time the button is clicked!
-  let listItems= " "
-  for (let i = 0; i < myLeads.length; i++) {
-    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-
-    //OTHER METHOD FOR THE ABOVE LINE:
-    //const li= document.createElement("li")
-    //li.textContent=myLeads[i]
-    //ulEl.append(li)
-    listItems += "<li>"+ myLeads[i] +"</li>"
-  }
-  ulEl.innerHTML= listItems;
-});   
+function renderLead(){
+  let listItem="<li>"+ inputEl.value +"</li>"
+  ulEl.innerHTML += listItem
+}
