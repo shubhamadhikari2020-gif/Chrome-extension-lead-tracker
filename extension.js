b0baaecd4891a542e272329ef55cd563bb343079
Shubham <shubhamadhikari2020@gmail.com>
2026-06-14 08:34:21 +0530

    made the code dynamic allowing it to take any data and fixed empty submit bug

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
@@ -11,7 +12,7 @@ console.log(leadsFromLocalStorage)
 // 2. If there are leads in storage, restore them back into our array and display them
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
 
 deleteBtn.addEventListener("dblclick",function(){ //dblclick triggers with double click
@@ -20,27 +21,29 @@ deleteBtn.addEventListener("dblclick",function(){ //dblclick triggers with doubl
     //  program would still hold onto the old links in its working memory until you refreshed the page.
 
     myLeads=[] //assigns a brand-new, completely empty array ([]) to your myLeads variable.
-    renderLeads() //Inside renderLeads(), you have a loop that builds HTML based on whatever is currently inside myLeads. 
+    render(myLeads) //Inside renderLeads(), you have a loop that builds HTML based on whatever is currently inside myLeads. 
     // Because you just set myLeads = [] on the line right above it, the loop finds nothing to loop through. As a result, 
     // listItems remains an empty string (""), and ulEl.innerHTML = listItems; clears all the list items off your screen.
 })
 
 inputBtn.addEventListener("click", function() {
+    if(inputEl.value){ //this if condition make sure any empty string is not passed into myLeads() array
     // 3. Push the new input value into the array
-    myLeads.push(inputEl.value);
+        myLeads.push(inputEl.value);
     
     // 4. Clear out the input field right away
-    inputEl.value = "";
+        inputEl.value = "";
     
     // 5. SAVING: Save the updated array to localStorage as a string
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
     
     // 6. Call the function to redraw the UI
-    renderLeads();
+        render(myLeads);
+    }
 });
 
 // 7. Dedicated function to handle rendering the UI cleanly
-function renderLeads() {
+function render() {
     let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
